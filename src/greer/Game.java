df2c5f86808b255e778cac894805f778a9a4caac
Hunter Greer <huntergreer03@gmail.com>
2023-10-10 22:02:05 -0700

    Cleaned up errors in code

--- src/greer/Game.java
+++ src/greer/Game.java
@@ -12,45 +12,6 @@ public class Game {
 	private ChessBoard boardObject = new ChessBoard();
 	
 	public Game(JFrame frame) {
-<<<<<<< HEAD
-		Piece whitePawn1 = new Piece(boardObject, frame, "A6", "/whitePawn.png", "WP");
-		Piece whitePawn2 = new Piece(boardObject, frame, "B6", "/whitePawn.png", "WP");
-		//Piece whitePawn3 = new Piece(boardObject, frame, "C2", "/whitePawn.png", "WP");
-		//Piece whitePawn4 = new Piece(boardObject, frame, "D2", "/whitePawn.png", "WP");
-		//Piece whitePawn5 = new Piece(boardObject, frame, "E2", "/whitePawn.png", "WP");
-		//Piece whitePawn6 = new Piece(boardObject, frame, "F2", "/whitePawn.png", "WP");
-		//Piece whitePawn7 = new Piece(boardObject, frame, "G2", "/whitePawn.png", "WP");
-		//Piece whitePawn8 = new Piece(boardObject, frame, "H2", "/whitePawn.png", "WP");
-		//
-		//Piece blackPawn1 = new Piece(boardObject, frame, "A7", "/blackPawn.png", "BP");
-		//Piece blackPawn2 = new Piece(boardObject, frame, "B7", "/blackPawn.png", "BP");
-		//Piece blackPawn3 = new Piece(boardObject, frame, "C7", "/blackPawn.png", "BP");
-		//Piece blackPawn4 = new Piece(boardObject, frame, "D7", "/blackPawn.png", "BP");
-		//Piece blackPawn5 = new Piece(boardObject, frame, "E7", "/blackPawn.png", "BP");
-		//Piece blackPawn6 = new Piece(boardObject, frame, "F7", "/blackPawn.png", "BP");
-		Piece blackPawn7 = new Piece(boardObject, frame, "G3", "/blackPawn.png", "BP");
-		Piece blackPawn8 = new Piece(boardObject, frame, "H3", "/blackPawn.png", "BP");
-		
-		//Piece whiteRook1 = new Piece(boardObject, frame, "A1", "/whiteRook.png", "WR");
-		//Piece whiteRook2 = new Piece(boardObject, frame, "H1", "/whiteRook.png", "WR");
-		//Piece blackRook1 = new Piece(boardObject, frame, "A8", "/blackRook.png", "BR");
-		//Piece blackRook2 = new Piece(boardObject, frame, "H8", "/blackRook.png", "BR");
-		//
-		//Piece whiteKnight1 = new Piece(boardObject, frame, "B1", "/whiteKnight.png", "WKn");
-		//Piece whiteKnight2 = new Piece(boardObject, frame, "G1", "/whiteKnight.png", "WKn");
-		//Piece blackKnight1 = new Piece(boardObject, frame, "B8", "/blackKnight.png", "BKn");
-		//Piece blackKnight2 = new Piece(boardObject, frame, "G8", "/blackKnight.png", "BKn");
-		//
-		//Piece whiteBishop1 = new Piece(boardObject, frame, "C1", "/whiteBishop.png", "WB");
-		//Piece whiteBishop2 = new Piece(boardObject, frame, "F1", "/whiteBishop.png", "WB");
-		//Piece blackBishop1 = new Piece(boardObject, frame, "F8", "/blackBishop.png", "BB");
-		//Piece blackBishop2 = new Piece(boardObject, frame, "C8", "/blackBishop.png", "BB");
-		//
-		Piece whiteKing = new Piece(boardObject, frame, "E1", "/whiteKing.png", "WK");
-		Piece blackKing = new Piece(boardObject, frame, "E8", "/blackKing.png", "BK");
-		//Piece whiteQueen = new Piece(boardObject, frame, "D1", "/whiteQueen.png", "WQ");
-		//Piece blackQueen = new Piece(boardObject, frame, "D8", "/blackQueen.png", "BQ");
-=======
 		Piece whitePawn1 = new Piece(boardObject, frame, "A2", "/whitePawn.png", "WP");
 		Piece whitePawn2 = new Piece(boardObject, frame, "B2", "/whitePawn.png", "WP");
 		Piece whitePawn3 = new Piece(boardObject, frame, "C2", "/whitePawn.png", "WP");
@@ -88,7 +49,6 @@ public class Game {
 		Piece blackKing = new Piece(boardObject, frame, "E8", "/blackKing.png", "BK");
 		Piece whiteQueen = new Piece(boardObject, frame, "D1", "/whiteQueen.png", "WQ");
 		Piece blackQueen = new Piece(boardObject, frame, "D8", "/blackQueen.png", "BQ");
->>>>>>> master
 		
 		
 		
